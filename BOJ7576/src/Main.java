8cd7e960fa4dd810052c31ba8a3986c88f302957
ub611 <ub2n611@gmail.com>
2020-01-16 23:38:53 +0900

    200116 complete

--- BOJ7576/src/Main.java
+++ BOJ7576/src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 	static int map[][];
@@ -6,52 +6,63 @@ public class Main {
 	static int dx[] = {0, 0, -1, 1};
 	static int dy[] = {1, -1, 0, 0};
 	static int ans;
+	
+	static class Point{
+		int x;
+		int y;
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int y = sc.nextInt();
 		int x = sc.nextInt();
-
 		map = new int[x][y];
 		dp = new int[x][y];
-		ans = 0;
-
+		int ans = 0;
+		
+		Queue<Point> q = new LinkedList<Point>();
+		
 		for(int i = 0; i < x; i++) {
 			for(int j = 0; j < y; j++) {
 				map[i][j] = sc.nextInt();
-				if(map[i][j] == 1)
-					dfs(i, j);
+				if(map[i][j] == 1) {
+					q.add(new Point(i, j));
+				}
 			}
+		}	
+		
+		//최소경로를 찾아야하기 때문에 bfs 사용
+		while(!q.isEmpty()) {
+			Point tmp = q.poll();
+			
+			for(int i = 0; i < 4; i++) {
+				int tx = tmp.x + dx[i];
+				int ty = tmp.y + dy[i];
+				
+				if(tx >= 0 && ty >= 0 && tx < x && ty < y && map[tx][ty] == 0) {
+					map[tx][ty] = 1;
+					q.add(new Point(tx, ty));
+					dp[tx][ty] = dp[tmp.x][tmp.y] + 1;		//얘 떄문에 오래걸림 ㅠ 
+					ans = dp[tx][ty];
+				}	
+			}	
 		}
-
-//		if(check()) {
-//			System.out.println("0");
-//			return;
-//		}
+		
+//		print(map);
+//		print(dp);
 		
 		if(!check())
 			System.out.println("-1");
 		else 
 			System.out.println(ans);
-
-
 	}
 
-	public static void dfs(int x, int y) {
-		if(x < 0 || y < 0 || x >= map.length || y >= map[0].length || map[x][y] == -1 || dp[x][y] == 1 || check())
-			return ;
-
-		dp[x][y] = 1;
-		map[x][y] = 1;
-
-		for(int i = 0; i < 4; i++) {
-			dfs(x + dx[i], y + dy[i]);
-		}
-
-		ans++;
-
-	}
+	
 
 	public static boolean check() {		//다 익었나 확인
 		for(int i = 0; i < map.length; i++) {
